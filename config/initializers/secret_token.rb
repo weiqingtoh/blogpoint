147c79910c195101e67e7cfbb7f15442d1b0e586
Weiqing <weiqingtoh@gmail.com>
2014-06-10 22:18:22 +0800

    rails new app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogpoint::Application.config.secret_token = 'b03065ed9378fa40d678837f0c40ac0700f5b97c8cd53a4f47f071a9a715c10e25fad642df4041e36b6be1669d4945d3b40810777e45a9c6bfcbe859119961a0'
