9d7caf75ad10950c60c7cb2d9df5fee5aa2c96b2
Weiqing <weiqingtoh@gmail.com>
2014-06-11 19:02:20 +0800

    minor edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-	http_basic_authentication_with name: 'tww', password: 'ckl', except: [:index, :show]
+	http_basic_authenticate_with name: 'tww', password: 'ckl', except: [:index, :show]
 
 	def show
 		@article = Article.find(params[:id])
