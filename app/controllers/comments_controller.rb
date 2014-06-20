6ca82531a9ec1a215b48e9ae7037106a177b2eaa
Weiqing <weiqingtoh@gmail.com>
2014-06-21 06:33:49 +0800

    styling changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
 		@comment = @article.comments.find(params[:id])
 		@comment.destroy
 		redirect_to article_path(@article)
-	end
+end
 
 	private
 
