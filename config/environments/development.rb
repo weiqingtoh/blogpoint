2af0817c8ab60c520a82e9eee855ee54712c37e4
Weiqing <weiqingtoh@gmail.com>
2014-06-23 17:08:35 +0800

    installed devise, removed welcome page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Blogpoint::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # For devise usage
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Blogpoint::Application.routes.draw do
-  get "welcome/index"
+  devise_for :users
 
-  root :to => 'welcome#index'
+  root :to => 'articles#index'
 
   resources :articles do 
     resources :comments
