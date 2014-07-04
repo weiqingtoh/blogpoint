8c645359a4a135b01f4c434763ef48611b9edd46
Weiqing <weiqingtoh@gmail.com>
2014-07-05 02:17:05 +0800

    added title capitaliser

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ActiveRecord::Base
+	before_save :capitalise_title
   attr_accessible :text, :title
   validates :title, presence: true, length: { minimum: 5 }
   has_many :comments, dependent: :destroy
+
+  def capitalise_title()
+  	self.title = self.title.split(' ').collect {|word| word.capitalize}.join(" ")
+  end
 end
